09a681b1a3eeada4e05b75507da7fb92b20820b5
Guilherme de Lima Conceição <guilherme.lima@ed.company>
2021-02-11 11:09:24 -0300

    model de video

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertube/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "FlutterTube",
-      home: Home(),
+      home: HomeScreen(),
     );
   }
 }
