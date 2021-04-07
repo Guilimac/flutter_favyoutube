16577f92f261df89b2dedea6c6786a49f79129d8
Guilherme de Lima Conceição <guilherme.lima@ed.company>
2021-04-07 20:24:45 -0300

    retorno de videos bloc

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertube/blocs/videos_bloc.dart';
 import 'package:fluttertube/delegates/data_search.dart';
+import 'package:fluttertube/widgets/video_tile.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -27,11 +30,27 @@ class HomeScreen extends StatelessWidget {
               icon: Icon(Icons.search),
               onPressed: () async{
                 String result = await showSearch(context: context, delegate: DataSearch());
-              }
+                if (result != null) BlocProvider.of<VideosBloc>(context).inSearch.add(result);
+                }
           )
         ],
+
       ),
-      body: Container(),
+      backgroundColor: Colors.black,
+      body: StreamBuilder(
+        stream: BlocProvider.of<VideosBloc>(context).outVideos,
+        builder: (context,snapshot){
+          if(snapshot.hasData)
+            return ListView.builder(
+                itemBuilder: (contaxt, index){
+                  return VideoTile(snapshot.data[index]);
+                },
+              itemCount: snapshot.data.length,
+            );
+          return Container();
+        },
+
+      ),
     );
   }
 }
